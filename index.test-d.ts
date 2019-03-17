26841468493c2938877657575b75dd7fc2d3b15d
Satya Rohith <gsatyarohith@gmail.com>
2019-03-17 20:46:46 +0530

    test: add tests for type definitions

---
+++ index.test-d.ts
@@ -0,0 +1,24 @@
+import {expectType} from 'tsd-check';
+import {file, base64, buffer, devices} from '.';
+
+expectType<Promise<void>>(
+	file('https://github.com/sindresorhus/capture-website#readme', './page.png', {
+		cookies: [
+			{
+				name: 'id',
+				value: 'unicorn',
+				expires: Math.round(new Date('2018-10-21').getTime() / 1000)
+			}
+		]
+	})
+);
+
+expectType<Promise<string>>(
+	base64('https://github.com/sindresorhus/capture-website#readme')
+);
+
+expectType<Promise<Buffer>>(
+	buffer('https://github.com/sindresorhus/capture-website#readme')
+);
+
+expectType<string[]>(devices);
