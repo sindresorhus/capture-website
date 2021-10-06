be340a3102e210a63ab7567471acaec4d383a97d
ropel <ropel@ropel.it>
2021-10-06 12:21:38 +0200

    Added try/finally to avoid resource leaking
    page and browser must be always closed, even in case of exceptions

--- index.js
+++ index.js
@@ -174,232 +174,235 @@ const internalCaptureWebsite = async (input, options) => {
 		launchOptions.slowMo = 100;
 	}
 
-	const browser = options._browser || await puppeteer.launch(launchOptions);
-	const page = await browser.newPage();
+	var browser = null;
+	var page = null;
+	try {
+		// if something goes wrong, throwing an exception, the finally part ensures that page and browser are closed, avoiding resource leakings
+		browser = options._browser || await puppeteer.launch(launchOptions);
+		page = await browser.newPage();
 
-	if (options.preloadFunction) {
-		await page.evaluateOnNewDocument(options.preloadFunction);
-	}
-
-	await page.setJavaScriptEnabled(options.isJavaScriptEnabled);
-
-	if (options.debug) {
-		page.on('console', message => {
-			let {url, lineNumber, columnNumber} = message.location();
-			lineNumber = lineNumber ? `:${lineNumber}` : '';
-			columnNumber = columnNumber ? `:${columnNumber}` : '';
-			const location = url ? ` (${url}${lineNumber}${columnNumber})` : '';
-			console.log(`\nPage log:${location}\n${message.text()}\n`);
-		});
-
-		page.on('pageerror', error => {
-			console.log('\nPage error:', error, '\n');
-		});
-
-		// TODO: Add more events from https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#event-requestfailed
-	}
-
-	if (options.authentication) {
-		await page.authenticate(options.authentication);
-	}
-
-	if (options.cookies) {
-		const cookies = options.cookies.map(cookie => parseCookie(isHTMLContent ? 'about:blank' : input, cookie));
-		await page.setCookie(...cookies);
-	}
-
-	if (options.headers) {
-		await page.setExtraHTTPHeaders(options.headers);
-	}
-
-	if (options.userAgent) {
-		await page.setUserAgent(options.userAgent);
-	}
-
-	await page.setViewport(viewportOptions);
-
-	if (options.emulateDevice) {
-		if (!(options.emulateDevice in puppeteer.devices)) {
-			throw new Error(`The device name \`${options.emulateDevice}\` is not supported`);
+		if (options.preloadFunction) {
+			await page.evaluateOnNewDocument(options.preloadFunction);
 		}
 
-		await page.emulate(puppeteer.devices[options.emulateDevice]);
-	}
+		await page.setJavaScriptEnabled(options.isJavaScriptEnabled);
 
-	await page.emulateMediaFeatures([{
-		name: 'prefers-color-scheme',
-		value: options.darkMode ? 'dark' : 'light'
-	}]);
-
-	await page[isHTMLContent ? 'setContent' : 'goto'](input, {
-		timeout: timeoutInSeconds,
-		waitUntil: 'networkidle2'
-	});
-
-	if (options.disableAnimations) {
-		await page.evaluate(disableAnimations, options.disableAnimations);
-	}
-
-	if (Array.isArray(options.hideElements) && options.hideElements.length > 0) {
-		await page.addStyleTag({
-			content: `${options.hideElements.join(', ')} { visibility: hidden !important; }`
-		});
-	}
-
-	if (Array.isArray(options.removeElements) && options.removeElements.length > 0) {
-		await page.addStyleTag({
-			content: `${options.removeElements.join(', ')} { display: none !important; }`
-		});
-	}
-
-	if (options.clickElement) {
-		await page.click(options.clickElement);
-	}
-
-	const getInjectKey = (ext, value) => isUrl(value) ? 'url' : (value.endsWith(`.${ext}`) ? 'path' : 'content');
-
-	if (!options.isJavaScriptEnabled) {
-		// Enable JavaScript again for `modules` and `scripts`.
-		await page.setJavaScriptEnabled(true);
-	}
-
-	if (options.modules) {
-		await Promise.all(options.modules.map(module_ => {
-			return page.addScriptTag({
-				[getInjectKey('js', module_)]: module_,
-				type: 'module'
+		if (options.debug) {
+			page.on('console', message => {
+				let {url, lineNumber, columnNumber} = message.location();
+				lineNumber = lineNumber ? `:${lineNumber}` : '';
+				columnNumber = columnNumber ? `:${columnNumber}` : '';
+				const location = url ? ` (${url}${lineNumber}${columnNumber})` : '';
+				console.log(`\nPage log:${location}\n${message.text()}\n`);
 			});
-		}));
-	}
 
-	if (options.scripts) {
-		await Promise.all(options.scripts.map(script => {
-			return page.addScriptTag({
-				[getInjectKey('js', script)]: script
+			page.on('pageerror', error => {
+				console.log('\nPage error:', error, '\n');
 			});
-		}));
-	}
 
-	if (options.styles) {
-		await Promise.all(options.styles.map(style => {
-			return page.addStyleTag({
-				[getInjectKey('css', style)]: style
-			});
-		}));
-	}
-
-	if (options.waitForElement) {
-		await page.waitForSelector(options.waitForElement, {
-			visible: true,
-			timeout: timeoutInSeconds
-		});
-	}
-
-	if (options.beforeScreenshot) {
-		await options.beforeScreenshot(page, browser);
-	}
-
-	if (options.element) {
-		await page.waitForSelector(options.element, {
-			visible: true,
-			timeout: timeoutInSeconds
-		});
-		screenshotOptions.clip = await page.$eval(options.element, getBoundingClientRect);
-		screenshotOptions.fullPage = false;
-	}
-
-	if (options.delay) {
-		await page.waitForTimeout(options.delay * 1000);
-	}
-
-	if (options.scrollToElement) {
-		// eslint-disable-next-line unicorn/prefer-ternary
-		if (typeof options.scrollToElement === 'object') {
-			await page.$eval(options.scrollToElement.element, scrollToElement, options.scrollToElement);
-		} else {
-			await page.$eval(options.scrollToElement, scrollToElement);
-		}
-	}
-
-	if (screenshotOptions.fullPage) {
-		// Get the height of the rendered page
-		const bodyHandle = await page.$('body');
-		const bodyBoundingHeight = await bodyHandle.boundingBox();
-		await bodyHandle.dispose();
-
-		// Scroll one viewport at a time, pausing to let content load
-		const viewportHeight = viewportOptions.height;
-		let viewportIncrement = 0;
-		while (viewportIncrement + viewportHeight < bodyBoundingHeight) {
-			const navigationPromise = page.waitForNavigation({waitUntil: 'networkidle0'});
-			/* eslint-disable no-await-in-loop */
-			await page.evaluate(_viewportHeight => {
-				/* eslint-disable no-undef */
-				window.scrollBy(0, _viewportHeight);
-				/* eslint-enable no-undef */
-			}, viewportHeight);
-			await navigationPromise;
-			/* eslint-enable no-await-in-loop */
-			viewportIncrement += viewportHeight;
+			// TODO: Add more events from https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#event-requestfailed
 		}
 
-		// Scroll back to top
-		await page.evaluate(_ => {
-			/* eslint-disable no-undef */
-			window.scrollTo(0, 0);
-			/* eslint-enable no-undef */
-		});
+		if (options.authentication) {
+			await page.authenticate(options.authentication);
+		}
 
-		// Some extra delay to let images load
-		await page.waitForFunction(imagesHaveLoaded, {timeout: timeoutInSeconds});
-	}
+		if (options.cookies) {
+			const cookies = options.cookies.map(cookie => parseCookie(isHTMLContent ? 'about:blank' : input, cookie));
+			await page.setCookie(...cookies);
+		}
 
-	if (options.inset && !screenshotOptions.fullPage) {
-		const inset = {top: 0, right: 0, bottom: 0, left: 0};
-		for (const key of Object.keys(inset)) {
-			if (typeof options.inset === 'number') {
-				inset[key] = options.inset;
-			} else {
-				inset[key] = options.inset[key] || 0;
+		if (options.headers) {
+			await page.setExtraHTTPHeaders(options.headers);
+		}
+
+		if (options.userAgent) {
+			await page.setUserAgent(options.userAgent);
+		}
+
+		await page.setViewport(viewportOptions);
+
+		if (options.emulateDevice) {
+			if (!(options.emulateDevice in puppeteer.devices)) {
+				throw new Error(`The device name \`${options.emulateDevice}\` is not supported`);
 			}
+
+			await page.emulate(puppeteer.devices[options.emulateDevice]);
 		}
 
-		let clipOptions = screenshotOptions.clip;
+		await page.emulateMediaFeatures([{
+			name: 'prefers-color-scheme',
+			value: options.darkMode ? 'dark' : 'light'
+		}]);
 
-		if (!clipOptions) {
-			clipOptions = await page.evaluate(() => ({
-				x: 0,
-				y: 0,
-				/* eslint-disable no-undef */
-				height: window.innerHeight,
-				width: window.innerWidth
-				/* eslint-enable no-undef */
+		await page[isHTMLContent ? 'setContent' : 'goto'](input, {
+			timeout: timeoutInSeconds,
+			waitUntil: 'networkidle2'
+		});
+
+		if (options.disableAnimations) {
+			await page.evaluate(disableAnimations, options.disableAnimations);
+		}
+
+		if (Array.isArray(options.hideElements) && options.hideElements.length > 0) {
+			await page.addStyleTag({
+				content: `${options.hideElements.join(', ')} { visibility: hidden !important; }`
+			});
+		}
+
+		if (Array.isArray(options.removeElements) && options.removeElements.length > 0) {
+			await page.addStyleTag({
+				content: `${options.removeElements.join(', ')} { display: none !important; }`
+			});
+		}
+
+		if (options.clickElement) {
+			await page.click(options.clickElement);
+		}
+
+		const getInjectKey = (ext, value) => isUrl(value) ? 'url' : (value.endsWith(`.${ext}`) ? 'path' : 'content');
+
+		if (!options.isJavaScriptEnabled) {
+			// Enable JavaScript again for `modules` and `scripts`.
+			await page.setJavaScriptEnabled(true);
+		}
+
+		if (options.modules) {
+			await Promise.all(options.modules.map(module_ => {
+				return page.addScriptTag({
+					[getInjectKey('js', module_)]: module_,
+					type: 'module'
+				});
 			}));
 		}
 
-		const x = clipOptions.x + inset.left;
-		const y = clipOptions.y + inset.top;
-		const width = clipOptions.width - (inset.left + inset.right);
-		const height = clipOptions.height - (inset.top + inset.bottom);
-
-		if (width === 0 || height === 0) {
-			await page.close();
-
-			throw new Error('When using the `clip` option, the width or height of the screenshot cannot be equal to 0.');
+		if (options.scripts) {
+			await Promise.all(options.scripts.map(script => {
+				return page.addScriptTag({
+					[getInjectKey('js', script)]: script
+				});
+			}));
 		}
 
-		screenshotOptions.clip = {x, y, width, height};
+		if (options.styles) {
+			await Promise.all(options.styles.map(style => {
+				return page.addStyleTag({
+					[getInjectKey('css', style)]: style
+				});
+			}));
+		}
+
+		if (options.waitForElement) {
+			await page.waitForSelector(options.waitForElement, {
+				visible: true,
+				timeout: timeoutInSeconds
+			});
+		}
+
+		if (options.beforeScreenshot) {
+			await options.beforeScreenshot(page, browser);
+		}
+
+		if (options.element) {
+			await page.waitForSelector(options.element, {
+				visible: true,
+				timeout: timeoutInSeconds
+			});
+			screenshotOptions.clip = await page.$eval(options.element, getBoundingClientRect);
+			screenshotOptions.fullPage = false;
+		}
+
+		if (options.delay) {
+			await page.waitForTimeout(options.delay * 1000);
+		}
+
+		if (options.scrollToElement) {
+			// eslint-disable-next-line unicorn/prefer-ternary
+			if (typeof options.scrollToElement === 'object') {
+				await page.$eval(options.scrollToElement.element, scrollToElement, options.scrollToElement);
+			} else {
+				await page.$eval(options.scrollToElement, scrollToElement);
+			}
+		}
+
+		if (screenshotOptions.fullPage) {
+			// Get the height of the rendered page
+			const bodyHandle = await page.$('body');
+			const bodyBoundingHeight = await bodyHandle.boundingBox();
+			await bodyHandle.dispose();
+
+			// Scroll one viewport at a time, pausing to let content load
+			const viewportHeight = viewportOptions.height;
+			let viewportIncrement = 0;
+			while (viewportIncrement + viewportHeight < bodyBoundingHeight) {
+				const navigationPromise = page.waitForNavigation({waitUntil: 'networkidle0'});
+				/* eslint-disable no-await-in-loop */
+				await page.evaluate(_viewportHeight => {
+					/* eslint-disable no-undef */
+					window.scrollBy(0, _viewportHeight);
+					/* eslint-enable no-undef */
+				}, viewportHeight);
+				await navigationPromise;
+				/* eslint-enable no-await-in-loop */
+				viewportIncrement += viewportHeight;
+			}
+
+			// Scroll back to top
+			await page.evaluate(_ => {
+				/* eslint-disable no-undef */
+				window.scrollTo(0, 0);
+				/* eslint-enable no-undef */
+			});
+
+			// Some extra delay to let images load
+			await page.waitForFunction(imagesHaveLoaded, {timeout: timeoutInSeconds});
+		}
+
+		if (options.inset && !screenshotOptions.fullPage) {
+			const inset = {top: 0, right: 0, bottom: 0, left: 0};
+			for (const key of Object.keys(inset)) {
+				if (typeof options.inset === 'number') {
+					inset[key] = options.inset;
+				} else {
+					inset[key] = options.inset[key] || 0;
+				}
+			}
+
+			let clipOptions = screenshotOptions.clip;
+
+			if (!clipOptions) {
+				clipOptions = await page.evaluate(() => ({
+					x: 0,
+					y: 0,
+					/* eslint-disable no-undef */
+					height: window.innerHeight,
+					width: window.innerWidth
+					/* eslint-enable no-undef */
+				}));
+			}
+
+			const x = clipOptions.x + inset.left;
+			const y = clipOptions.y + inset.top;
+			const width = clipOptions.width - (inset.left + inset.right);
+			const height = clipOptions.height - (inset.top + inset.bottom);
+
+			if (width === 0 || height === 0) {
+				throw new Error('When using the `clip` option, the width or height of the screenshot cannot be equal to 0.');
+			}
+
+			screenshotOptions.clip = {x, y, width, height};
+		}
+
+		const buffer = await page.screenshot(screenshotOptions);
+		return buffer;
+	}  finally {
+		if(page) {
+			await page.close();
+		}
+		if (!options._keepAlive && browser) {
+			await browser.close();
+		}
 	}
-
-	const buffer = await page.screenshot(screenshotOptions);
-
-	await page.close();
-
-	if (!options._keepAlive) {
-		await browser.close();
-	}
-
-	return buffer;
 };
 
 const captureWebsite = {};
